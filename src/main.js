c03cf4d6970b234d65b2d2558ca85bb16c32ed1c
Ellie <ellieslife@icloud.com>
2024-04-09 16:46:02 -0300

    .

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { k } from "./kaboomCtx";
+import { scaleFactor } from "./constants";
 
 k.loadSprite("spritesheet", "./spritesheet.png", {
   sliceX: 39,
@@ -12,3 +13,25 @@ k.loadSprite("spritesheet", "./spritesheet.png", {
     "walk-up": { from: 1030, to: 1033, loop: true, speed: 8 },
   },
 });
+
+k.loadSprite("map", "./map.png");
+
+k.setBackground(k.Color.fromHex("#0a0121"));
+
+k.scene("main", async () => {
+  const mapData = await (await fetch("./map.json")).json();
+  const layers = mapData.layers;
+
+  const map = k.make([k.sprite("map"), k.pos(0), k.scale(scaleFactor)]);
+
+  const player = k.make([
+    k.sprite("spritesheet", { anim: "idle-down" }),
+    k.area({
+      shape: new k.Rect(k.vec2(0, 3), 10, 10), // can edit this is the players hit box (custom)
+    }),
+    k.body(),
+    k.anchor("center"), // left off here 49:30  https://www.youtube.com/watch?v=wy_fSStEgMs&t=1560s
+  ]);
+});
+
+k.go("main");
