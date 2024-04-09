7a12b198070565ac2cf4bf4998bd761c1df00909
Ellie <ellieslife@icloud.com>
2024-04-09 15:34:06 -0300

    add animations

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ k.loadSprite("spritesheet", "./spritesheet.png", {
   sliceX: 39,
   sliceY: 31,
   anims: {
-    "idle-down": 936,
+    "idle-down": 952,
+    "walk-down": { from: 952, to: 955, loop: true, speed: 8 },
+    "idle-side": 991,
+    "walk-side": { from: 991, to: 994, loop: true, speed: 8 },
+    "idle-up": 1030,
+    "walk-up": { from: 1030, to: 1033, loop: true, speed: 8 },
   },
 });
